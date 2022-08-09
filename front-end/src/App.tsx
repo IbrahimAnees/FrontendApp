ecba2c2c633644c6604c0144cb4604c9095f39e4
IbrahimAnees <ibrahimanees2002@gmail.com>
2022-08-09 12:35:14 +1200

    Changed to digimon

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,37 +1,56 @@
-import './App.css';
-import { useState } from 'react';
-import axios from 'axios';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
+import Button from '@mui/material/Button';
+import { CenterFocusStrong } from "@mui/icons-material";
 
 function App() {
-  const [quote, setQuote] = useState('')
-  const [nameEntered, setName] = useState("");
+  const [digimonName, setDigimonName] = useState("");
+  const [digimonPic, setPicURL] = useState<undefined | any>(undefined);
 
-  const getQuote = () => {
-    axios.get('https://api.agify.io?name=' + nameEntered)
-    .then(res => {
-      if (res.data.age !== null) {      console.log(res.data.age)
-        setQuote(res.data.age)}
-      else {
-        setQuote('Not a valid name!')
-      }
-    }).catch(err => {
-      console.log(err)
-    })
-  }
+  const DIGIMON_BASE_API_URL = "https://digimon-api.vercel.app/api/digimon";
   return (
-    <div className="App">
+    <div style={{color: "blue"}}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+      <h1 style={{color: "darkgreen"}}> Digimon Images </h1>
+      </div>
+      <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+        <h2>Enter the digimon's name:</h2>
+        <br></br> <br></br>
 
-<input
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+        <input 
           type="text"
-          id="pokemon-name"
-          name="pokemon-name"
-          onChange={(e) => setName(e.target.value)}
+          id="digimon-name"
+          name="digimon-name"
+          onChange={(e) => setDigimonName(e.target.value)}
         />
-
-      <button onClick={getQuote}>Get Quote</button>
-      { quote && <p>{quote}</p> }
+        <br />
+        <Button onClick={searchForDigimon} variant="contained">Search</Button>
+        </div>
+      </div>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <h3>Digimon entered: {digimonName}</h3>
+      </div>
+      {digimonPic === undefined ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+        <h3 style={{color: "red"}}>Digimon does not exist!</h3>
+        </div>
+      ) : (
+        <div id="digimon-result" style={{ display: "flex", justifyContent: "center" }}>
+          <img src={digimonPic} />
+        </div>
+      )}
     </div>
   );
+
+  function searchForDigimon() {
+    axios.get(DIGIMON_BASE_API_URL + '/name/' + digimonName).then((res) => {
+      setPicURL(res.data[0].img);
+    });
+  }
 }
 
- export default App;
+export default App;
